43b847713cd7b8cd341fccd8e585e30d45b42bef
Tarik Raposo <tarikraposo@gmail.com>
2024-06-05 12:58:16 -0300

    add more page, change style

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const more = (document.getElementById("more").onclick = function () {
+  location.href = "/src/pages/more.html";
+});
+
+function handleGoToPage(link) {
+  onclick((location.href = `${link}`));
+}
+
+const pokedex = "https://github.com/tarikraposo/pokedex-simples";
+const myjam = "https://github.com/tarikraposo/MyJam-front/settings/pages";
+
